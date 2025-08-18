ce8c86014bb7edd15ea747975f50a041bca25f97
Chris Carlon <cmcarleone@gmail.com>
2025-08-18 18:16:51 +0100

    chore: v0.1.0

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use muy_zipido::{
 };
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let url = "https://api.os.uk/downloads/v1/products/CodePointOpen/downloads?area=GB&format=GeoPackage&redirect";
+    let url = "URL_HERE";
     println!("Fetching and processing ZIP from: {}", url);
 
     let extractor = MuyZipido::new(url, 10240)?.with_progress(Style::Blocks, Colour::Magenta);
